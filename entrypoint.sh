6c348b9536e3edd7be79800687b898436f635c41
BlueKrow <ndioses@gmail.com>
2020-03-04 20:41:55 +0100

    feat: process input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -l
 
-echo "AUTH_TOKEN: ${{ secrets.GITHUB_TOKEN }}"
-echo "REPO_OWNER_AND_NAME: ${{ github.REPOSITORY }}"
-echo "REPO_URL: ${{ github.EVENT.REPOSITORY.CLONE_URL }}"
-echo "OWNER_NAME: ${{ github.EVENT.REPOSITORY.OWNER.NAME }}"
-echo "OWNER_EMAIL: ${{ github.EVENT.REPOSITORY.OWNER.EMAIL }}"
-echo "COMMIT_MESSAGE: ${{ github.EVENT.HEAD_COMMIT.MESSAGE }}"
-echo "DEPLOY_BRANCH: gh-pages"
-echo "DEPLOY_DIR: .deploy"
-echo "SOURCE_DIR: public"
+echo $1
+#echo "AUTH_TOKEN: ${{ secrets.GITHUB_TOKEN }}"
+#echo "REPO_OWNER_AND_NAME: ${{ github.REPOSITORY }}"
+#echo "REPO_URL: ${{ github.EVENT.REPOSITORY.CLONE_URL }}"
+#echo "OWNER_NAME: ${{ github.EVENT.REPOSITORY.OWNER.NAME }}"
+#echo "OWNER_EMAIL: ${{ github.EVENT.REPOSITORY.OWNER.EMAIL }}"
+#echo "COMMIT_MESSAGE: ${{ github.EVENT.HEAD_COMMIT.MESSAGE }}"
+#echo "DEPLOY_BRANCH: gh-pages"
+#echo "DEPLOY_DIR: .deploy"
+#echo "SOURCE_DIR: public"
