eb4b3ca7d7b4a6b32b1d8dafa3640827c345ff04
BlueKrow <ndioses@gmail.com>
2020-03-08 21:07:23 +0100

    chore: add calc vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,21 @@
 
 echo "ENVIRONMENT VARIABLES"
 echo "Token:$INPUT_TOKEN"
-echo "RepoQualifiedName: $INPUT_REPO_QUALIFIED_NAME"
-echo "RepoUrl: $INPUT_REPO_URL"
+echo "RepoName: $INPUT_REPO_NAME"
 echo "OwnerName: $INPUT_OWNER_NAME"
 echo "OwnerEMail: $INPUT_OWNER_EMAIL"
 echo "CommitMessage: $INPUT_COMMIT_MESSAGE"
 echo "DeployBranch: $INPUT_DEPLOY_BRANCH"
 echo "DeployDir: $INPUT_DEPLOY_DIR"
 echo "SourceDir: $INPUT_SOURCE_DIR"
-pwd
-ls
+
+echo "CALCULATED VARIABLES"
+REPO_URL=https://github.com/${INPUT_OWNER_NAME}/${INPUT_REPO_NAME}.git
+echo "RepoUrl: $REPO_URL"
+REPO_URL_AUTH=https://${INPUT_OWNER_NAME}:${INPUT_TOKEN}@github.com/${INPUT_OWNER_NAME}/${INPUT_REPO_NAME}.git
+echo "RepoUrlAuth: $REPO_URL_AUTH"
+DEPLOY_BRANCH_EXISTS=$(git ls-remote --heads ${REPO_URL} ${INPUT_DEPLOY_BRANCH} | wc -l)
+echo "DeployUrlExists: $DEPLOY_BRANCH_EXISTS"
+
+git -v
+npm -v
